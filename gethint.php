cce241bf7eaa4d5974a86b0d4459e5f26e7c7428
Al-sany Samil <alsanysamil@gmail.com>
2020-04-06 11:57:29 +0600

    PHP file to connect to database and get districts to create dropdown dynamically

--- gethint.php
+++ gethint.php
@@ -1,36 +1,36 @@
  <?php
-	$k[] = "Khulna";
-	$k[] = "Jessore";
-	$k[] = "Magura";
-	$k[] = "Kushtia";
-	$k[] = "Jjenaidha";
-	$k[] = "Chuadanga";
-	$k[] = "Satkhira";
-
-	$d[] = "Dhaka";
-	$d[] = "Gazipur";
-	$d[] = "Kishoreganj";
-	$d[] = "Tangail";
-	$d[] = "Manikganj";
 
 	$q = $_REQUEST["q"];
 
 	$hint = "";
 
 	if ($q !== "/") {
-	  $q = strtolower($q);
-	  $hint="<select>";
-	  if($q=="khulna"){
-	  foreach($k as $name) {
-	      $hint=$hint."<option>".$name."</option>";
-	    }
-	  }
-	  if($q=="dhaka"){
-	  foreach($d as $name) {
-	      $hint=$hint."<option>".$name."</option>";
-	    }	    
-	  }
-	  $hint=$hint."</select>";
+		$q = strtolower($q);
+		$hint="<select>";
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "myDB";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+
+		$sql = "SELECT * FROM Divisions WHERE division='$q'";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+	    	// output data of each row
+	    	while($row = $result->fetch_assoc()) {
+	        	$hint=$hint ."<option>" .$row["district"]. "</option>" ;
+	    	}
+		}
+		$conn->close();
+	
+	  	$hint=$hint."</select>";
 	}
 	echo $hint === "" ? "<select><option>Districts</option></select>" : $hint;
 ?> 
